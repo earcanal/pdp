0945bdfecce31e687ee7d945f3f3466e1314a400
earcanal <paul@miraclefish.com>
2014-11-17 18:49:58 +0000

    Converted Octave syntax so code runs on Matlab.

--- multiple_nonorthogonal_prototypes.m
+++ multiple_nonorthogonal_prototypes.m
@@ -1,64 +1,14 @@
-% "Multiple, nonorthogonal prototypes" (McClelland & Rumelhart, 1985 p.168ff)
-% 16 "vision" elements: distinguish between cats, dogs and bagels
-% 8 "name" elements: distinguish between 'cat', 'dog' and 'bagel'
+% 'Multiple, nonorthogonal prototypes' (McClelland & Rumelhart, 1985 p.168ff)
+% 16 'vision' elements: distinguish between cats, dogs and bagels
+% 8 'name' elements: distinguish between 'cat', 'dog' and 'bagel'
 
-1; % not a function file
-
-function A=activations(a,units)
-  A = ones(units,"single") .* repmat(a,units,1);
-  A = A - diag(diag(A)); % 0 -> diagonal
-endfunction
-
-function display (ticks, e, a)
-  printf("\tinput  ");
-  printf("%5.1f",e(:));
-  printf("\n");
-  printf("\toutput ");
-  printf("%5.1f",a(:));
-  printf(" (stable after %d ticks)\n",ticks);
-endfunction
-
-function newa = test (a,e,W,units,ddb)
-  % FIXME: what should this constant be?
-  E = D = single(.27);        % Excitation/Decay
-  max_ticks = single(50);     % maximum iterations for activations to stabilise
-  precision = 1000;           % overcome floating point comparison problem
-  A = activations(a,units);   % activations as a matrix
-  A = A .* W;                 % weighted activations
-  n = sum(A,2) + e';          % phase 1: determine net activations
-  if (ddb)
-    e
-    a
-  endif
-  ticks = 0;
-  do                          % phase 2: update activations
-    old_a = a;
-    for i = 1:units
-      ni = n(i);
-      if (ni > 0)
-        d = E * ni * (1 - a(i)) - D * a(i);
-      else % ni <= 0
-        d = E * ni * (a(i) + 1) - D * a(i);
-      endif
-      a(i) = a(i) + d;
-    endfor
-    ticks++;
-    ra  = round(a * precision);
-    roa = round(old_a * precision);
-    if (ddb)
-      ra
-      roa
-    endif
-  until (isequal(ra,roa) | ticks == max_ticks) % stable activation
-  newa = a;
-  display(ticks,e,newa);
-endfunction
+function multiple_nonorthoganal_prototypes
 
 %% constants
 ddb   = 0;                       % DEBUG = 1 / NO_DEBUG = 0
 units = single(8);               % number of units in the module
-W     = ones(units,"single");    % initial weights
-a     = zeros(1,units,"single"); % initial activations
+W     = ones(units,'single');    % initial weights
+a     = zeros(1,units,'single'); % initial activations
 S     = single(2.9);               % global strength
 e = single([1 -1 1 -1 1 1 -1 -1]);      % external pattern
 % FIXME: weight decay ???
@@ -66,11 +16,11 @@ e = single([1 -1 1 -1 1 1 -1 -1]);      % external pattern
 a = test(a,e,W,units,ddb);
 
 for trial = 1:10
-  printf("learning trial %d\n",trial);
+  fprintf('learning trial %d\n',trial);
   a = test(a,e,W,units,ddb);
 
   %% delta rule
-  if (ddb) printf("** Applying delta rule **\n\n"); endif
+  if (ddb) fprintf('** Applying delta rule **\n\n'); end
   A      = activations(a,units);    % new activations as a matrix
   A      = A .* W;                  % weighted activations
   delta  = e' - sum(A,2);
@@ -79,5 +29,62 @@ for trial = 1:10
   if (ddb)
     W
     deltas
-  endif
-endfor
+  end
+end
+end
+
+function A=activations(a,units)
+  A = ones(units,'single') .* repmat(a,units,1);
+  A = A - diag(diag(A)); % 0 -> diagonal
+end
+
+function display (ticks, e, a)
+  fprintf('\tinput  ');
+  fprintf('%5.1f',e(:));
+  fprintf('\n');
+  fprintf('\toutput ');
+  fprintf('%5.1f',a(:));
+  fprintf(' (stable after %d ticks)\n',ticks);
+end
+
+function newa=test(a,e,W,units,ddb)
+  % FIXME: what should this constant be?
+  D = single(.27);        % Excitation/Decay
+  E = D;
+  max_ticks = single(50);     % maximum iterations for activations to stabilise
+  precision = 1000;           % overcome floating point comparison problem
+  A = activations(a,units);   % activations as a matrix
+  A = A .* W;                 % weighted activations
+  n = sum(A,2) + e';          % phase 1: determine net activations
+  if (ddb)
+    fprintf('%s',mat2str(e));
+    fprintf('%s',mat2str(a));
+  end
+  
+  % phase 2: update activations
+  ticks = 0;
+  ra  = 1;
+  roa = 0;
+  while (not(isequal(ra,roa)) || ticks < max_ticks) % stable activation
+    old_a = a;
+    for i = 1:units
+      ni = n(i);
+      if (ni > 0)
+        d = E * ni * (1 - a(i)) - D * a(i);
+      else % ni <= 0
+        d = E * ni * (a(i) + 1) - D * a(i);
+      end
+      a(i) = a(i) + d;
+      end
+    ticks = ticks + 1;
+    ra  = round(a * precision);
+    roa = round(old_a * precision);
+    if (ddb)
+      fprintf('%s',ra);
+      fprintf('%s',roa);
+    end
+  end % stable activation
+  newa = a;
+  display(ticks,e,newa);
+end
+

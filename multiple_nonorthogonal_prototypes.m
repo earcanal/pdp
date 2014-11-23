ff90fffa7dc31817025ff7c97a2feedb4f0b14de
earcanal <paul@miraclefish.com>
2014-11-23 11:08:41 +0000

    FIX/BUG: Implementation misunderstandings.
    Fixed some misinterpretations of M&R(1985) but getting some Inf values
    during the calculations so something clearly still isn't right!

--- multiple_nonorthogonal_prototypes.m
+++ multiple_nonorthogonal_prototypes.m
@@ -6,9 +6,9 @@ function multiple_nonorthogonal_prototypes
   %% constants
   ddb   = 0;                         % DEBUG level 0-2
   units = single(8);                 % number of units in the module
-  W     = zeros(units,'single');      % initial weights
+  W     = zeros(units,'single');     % initial weights
   a     = zeros(1,units,'single');   % initial activations
-  S     = single(.07);               % global strength
+  S     = single(.05);               % global strength
   e = single([1 -1 1 -1 1 1 -1 -1]); % external pattern
   % FIXME: weight decay ???
 
@@ -18,22 +18,28 @@ function multiple_nonorthogonal_prototypes
     fprintf('learning trial %d\n',trial);
     %% delta rule
     if (ddb) fprintf('** Applying delta rule **\n\n'); end
-    A      = activations(a,units);    % new activations as a matrix
-    A      = A .* W;                  % weighted activations
-    delta  = e' - sum(A,2);
-    deltas = activations(delta',units);
+    A     = activations(a,units);    % activations as a matrix
+    delta = e' - sum(A,2);
+    if (ddb)
+      fprintf('a(i)\n');
+      disp(A);
+      fprintf('e = %s\ni = %s\n',mat2str(e'),mat2str(sum(A,2),2));
+      fprintf('e - i = %s\n',mat2str(delta,2));
+    endif
+    deltas = repmat(delta,1,units);
+    deltas = deltas - diag(diag(deltas));
     W      = S .* deltas .* A;
     if (ddb)
-      fprintf('weights\n');
-      disp(W);
-      fprintf('deltas\n');
-      disp (deltas);
+    fprintf('deltas\n');
+    disp(deltas);
+    fprintf('weights\n');
+    disp(W);
     end
     a = test(a,e,W,units,ddb);
   end
-  % FIXME
-  % unit 8 distorted
-  % expected .1 / actual .66
+  % FIXME: partial input
+  a = test(a,[1 -1 1 -1 0 0 0 0],W,units,ddb);
+  % FIXME: unit 8 distorted
   a = test(a,[1 -1 1 -1 1 1 -1 1],W,units,ddb);
 end
 
@@ -43,47 +49,44 @@ function A=activations(a,units)
 end
 
 function display (ticks, e, a)
-  fprintf('\tinput  ');
+  fprintf('\te ');
   fprintf('%6.2f',e(:));
   fprintf('\n');
-  fprintf('\toutput ');
+  fprintf('\ta ');
   fprintf('%6.2f',a(:));
   fprintf('\n\n');
 end
 
 function newa=test(a,e,W,units,ddb)
-  E = single(.9);               % excitation
-  D = single(.9);               % decay
-  max_ticks = single(50);       % maximum iterations for activations to stabilise
-  precision = 1000;             % overcome floating point comparison problem
+  E = single(.99);            % excitation
+  D = single(.99);            % decay
+  max_ticks = single(50);     % maximum iterations for activations to stabilise
 
-  % phase 2: update activations
-  for tick = 1:max_ticks        % stable activation
-    A = activations(a,units);   % activations as a matrix
-    A = A .* W;                 % weighted activations
-    n = e' + sum(A,2);          % net activations
-    if (ddb)
-      fprintf('e = %s\n',mat2str(e,2));
+  for tick = 1:max_ticks      % ticks to stable activation
+    % phase 1: calculate net inputs for units
+    A = activations(a,units); % activations as a matrix
+    A = A .* W;               % weighted activations
+    n = sum(A,2) + e';        % net inputs 
+    if (ddb > 1)
+      %fprintf('e = %s\n',mat2str(e,2));
       fprintf('a = %s\n',mat2str(a,2));
-      fprintf('sum(A,2) = %s\n',mat2str(sum(A,2)));
-      fprintf('net activations = %s\n',mat2str(n));
+      %fprintf('sum(A,2) = %s\n',mat2str(sum(A,2)));
+      %fprintf('net activations = %s\n',mat2str(n));
     end
   
-    old_a = a;
+    % phase 2: update unit activations
     for i = 1:units
-      ni = n(i);
-      if (ni > 0)
-        d = E * ni * (1 - a(i)) - D * a(i);
+      if (n(i) > 0)
+        d = E * n(i) * (1 - a(i)) - D * a(i);
       else % ni <= 0
-        d = E * ni * (a(i) + 1) - D * a(i);
+        d = E * n(i) * (a(i) + 1) - D * a(i);
+      end
+      if (ddb > 1)
+        fprintf('d = %.2f\n',d);
       end
       a(i) = a(i) + d;
     end
-    if (ddb > 1)
-      fprintf('weights (tick %d)\n',tick);
-      disp(W);
-    end
   end
+  display(tick,e,a);
   newa = a;
-  display(tick,e,newa);
 end
